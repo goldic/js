0891b51905ed27c3576ce5af69d83b6f91974be3
denis <denis@begom.com>
2024-10-09 11:51:00 +0200

    added tests for Load, LoadObject

--- js_test.go
+++ js_test.go
@@ -106,6 +106,29 @@ func TestAnd(t *testing.T) {
 	require(t, res1 == true)
 }
 
+func TestLoad(t *testing.T) {
+
+	vv, err := Load("https://api.binance.com/api/v3/ticker/price")
+
+	tickers := map[string]float64{}
+	for _, obj := range vv.Objects() {
+		tickers[obj.GetStr("symbol")] = obj.GetNum("price")
+	}
+
+	require(t, err == nil)
+	require(t, len(tickers) > 100)
+	require(t, tickers["BTCUSDT"] > 10_000)
+}
+
+func TestLoadObject(t *testing.T) {
+
+	obj, err := LoadObject("https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT")
+
+	require(t, err == nil)
+	require(t, obj.GetStr("symbol") == "BTCUSDT")
+	require(t, obj.GetNum("price") > 10_000)
+}
+
 func require(t *testing.T, ok bool) {
 	if !ok {
 		t.Fail()
